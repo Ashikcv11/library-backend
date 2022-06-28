0b86a303f8a2e82cd15badf46781912bbbd8bc17
Ashikcv11 <ashikcv17@gmail.com>
2022-06-28 14:59:21 +0530

    security
    security

--- model/libraryModel.js
+++ model/libraryModel.js
@@ -3,28 +3,13 @@ const mongoose = require('mongoose')
 // const Mongoose = require('mongoose');
 
 const DB = "mongodb+srv://ashikcv:5s_xbvgUbwA33Av@cluster0.8ire35m.mongodb.net/?retryWrites=true&w=majority"
-mongoose.connect(DB,{ useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
- console.log("Database Connection Successful")
-}).catch((err)=>{
- console.log(err)
-})
+// mongoose.connect(DB,{ useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(()=>{
+        console.log("Database Connection Successful")
+    }).catch((err)=>{
+     console.log(err)
+    });
 
-//connect to mongodb
-// mongoose.connect('mongodb://localhost:27017/Library')
-
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
-
-//on connection
-// mongoose.connection.on('connected' ,()=>{
-//     console.log('connected to the mongoose database @ 27017')
-// })
-
-// mongoose.connection.on('error' ,()=>{
-//     if(error)
-//     {
-//         console.log('Error in database connection:'+error)
-//     }
-// });
 
 const Schema = mongoose.Schema;
 
--- model/userData.js
+++ model/userData.js
@@ -3,28 +3,14 @@ const mongoose = require('mongoose');
 // const Mongoose = require('mongoose');
 
 const DB = "mongodb+srv://ashikcv:5s_xbvgUbwA33Av@cluster0.8ire35m.mongodb.net/?retryWrites=true&w=majority"
-mongoose.connect(DB,{ useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
- console.log("Database Connection Successful")
-}).catch((err)=>{
- console.log(err)
-})
+// mongoose.connect(DB,{ useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(()=>{
+        console.log("Database Connection Successful")
+    }).catch((err)=>{
+     console.log(err)
+    });
+    
 
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
-
-//connect to mongodb
-// mongoose.connect('mongodb://localhost:27017/Library');
-
-//on connection
-// mongoose.connection.on('connected' ,()=>{
-//     console.log('connected to the mongoose database @ 27017 for the userdata')
-// })
-
-// mongoose.connection.on('error' ,()=>{
-//     if(err)
-//     {
-//         console.log('Error in database connection:'+err)
-//     }
-// });
 
 const Schema = mongoose.Schema;
 
