187b30aa6c0feb66467778735eb937d79ffcf975
Ashikcv11 <ashikcv17@gmail.com>
2022-06-28 10:43:20 +0530

    cloud changes
    cloud url added

--- model/libraryModel.js
+++ model/libraryModel.js
@@ -1,16 +1,23 @@
-// const mongoose = require('mongoose')
-const { default: mongoose } = require('mongoose');
-const Mongoose = require('mongoose');
+const mongoose = require('mongoose')
+// const { default: mongoose } = require('mongoose');
+// const Mongoose = require('mongoose');
+
+const DB = "mongodb+srv://ashikcv:5s_xbvgUbwA33Av@cluster0.8ire35m.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(DB,{ useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+ console.log("Database Connection Successful")
+}).catch((err)=>{
+ console.log(err)
+})
 
 //connect to mongodb
 // mongoose.connect('mongodb://localhost:27017/Library')
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 //on connection
-mongoose.connection.on('connected' ,()=>{
-    console.log('connected to the mongoose database @ 27017')
-})
+// mongoose.connection.on('connected' ,()=>{
+//     console.log('connected to the mongoose database @ 27017')
+// })
 
 // mongoose.connection.on('error' ,()=>{
 //     if(error)
